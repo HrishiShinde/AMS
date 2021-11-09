c215594d5eedb13b8df95ab75ef6d158a29800ac
Hrishikesh Shinde <Shindehrishikesh0115@gmail.com>
2021-11-09 20:04:27 +0530

    Idk but it's not a Last commit

--- AMS/settings.py
+++ AMS/settings.py
@@ -27,6 +27,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-tl-@w)j-ncg(vou8!e5h!q#if-r0k5l&um+iyucq%z-z3bb29('
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
